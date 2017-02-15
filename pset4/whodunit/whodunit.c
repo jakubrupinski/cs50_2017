d2e44157396552f05659fe19ac7d7e88f1716a82
Jakub Rupinski <rupek1995@gmail.com>
2017-02-15 02:11:53 +0100

    Remove unnecessary header and improve function description in whodunit

--- pset4/whodunit/whodunit.c
+++ pset4/whodunit/whodunit.c
@@ -4,7 +4,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "bmp.h"
 
@@ -77,7 +76,7 @@ int main(int argc, char *argv[])
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            // remove red from each pixel (if it's all red)
+            // if pixel is all red - change it to black
             if (triple.rgbtRed == 0xff)
             {
               triple.rgbtRed = 0x00;
