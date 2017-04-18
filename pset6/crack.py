9e957622bbf5b678a2b0c2dc6c7e5d303276294a
Jakub Rupinski <rupek1995@gmail.com>
2017-04-18 16:46:17 +0200

    Implement cracking 4-char lowercase passwords

--- pset6/crack.py
+++ pset6/crack.py
@@ -16,12 +16,44 @@ def main():
 def decrypt(encryptedText):
     # first two characters is salt
     salt = encryptedText[:2]
+
+    # Check for 1-char password
     for i in range(ord('a'), ord('z')):
         currString = chr(i)
         hashed = crypt.crypt(currString, salt)
         if hashed == encryptedText:
             print(currString)
-            break
+            exit(0)
+
+    # Check for 2-char password
+    for i in range(ord('a'), ord('z')):
+        for j in range(ord('a'), ord('z')):
+            currString = chr(i) + chr(j)
+            hashed = crypt.crypt(currString, salt)
+            if hashed == encryptedText:
+                print(currString)
+                exit(0)
+
+    # Check for 3-char password
+    for i in range(ord('a'), ord('z')):
+        for j in range(ord('a'), ord('z')):
+            for k in range(ord('a'), ord('z')):
+                currString = chr(i) + chr(j) + chr(k)
+                hashed = crypt.crypt(currString, salt)
+                if hashed == encryptedText:
+                    print(currString)
+                    exit(0)
+
+    # Check for 4-char password
+    for i in range(ord('a'), ord('z')):
+        for j in range(ord('a'), ord('z')):
+            for k in range(ord('a'), ord('z')):
+                for l in range(ord('a'), ord('z')):
+                    currString = chr(i) + chr(j) + chr(k) + chr(l)
+                    hashed = crypt.crypt(currString, salt)
+                    if hashed == encryptedText:
+                        print(currString)
+                        exit(0)
 
 
 if __name__ == '__main__':
